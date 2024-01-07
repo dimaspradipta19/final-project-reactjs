15cf2ca78187a30b3ce94e5486b89ba6f7df8ec9
Dimas Pradipta Putra <dimaspradiptaputra@Dimass-MacBook-Air.local>
2024-01-07 20:18:37 +0700

    update new reducer

---
+++ src/components/main content/rootReducer.js
@@ -0,0 +1,11 @@
+// rootReducer.js
+
+import { combineReducers } from 'redux';
+import cartReducer from './cartReducer';
+
+const rootReducer = combineReducers({
+    cart: cartReducer,
+    // other reducers can be added here
+});
+
+export default rootReducer;
